cba24328cd8a8b96a438ef2c047979e9b475e4d0
Blossom Babalola <sapashb@gmail.com>
2020-09-28 09:43:30 +0100

    run build

---
+++ build/precache-manifest.0bf502fdfa1a696858a442a921c764a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1724f74f6ee02d479111",
+    "url": "/static/css/main.b3224e76.chunk.css"
+  },
+  {
+    "revision": "1724f74f6ee02d479111",
+    "url": "/static/js/main.00b587d1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0b458c353e1af8476c72",
+    "url": "/static/js/2.0184a7a9.chunk.js"
+  },
+  {
+    "revision": "892f7463ea3980c12770384449eee2b8",
+    "url": "/index.html"
+  }
+];
